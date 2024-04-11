0b2158a0faa9770ffd512145d9e4cf44f4aab1a7
BHUMI1205 <bhumiitaliya.crawlapps@gmail.com>
2024-04-11 11:57:55 +0530

    seeder change

--- seeder/admin-seeder.js
+++ seeder/admin-seeder.js
@@ -3,7 +3,8 @@ import bcrypt from "bcrypt";
 import { user } from '../model/user.js'
 
 async function seedAdmin() {
-    await mongoose.connect('mongodb://127.0.0.1/Blogs');
+    // await mongoose.connect('mongodb://127.0.0.1/Blogs');
+    await mongoose.connect('process.env.MONGO_URI');
 
     const existingAdmin = await user.findOne({ role: "superAdmin" });
 
