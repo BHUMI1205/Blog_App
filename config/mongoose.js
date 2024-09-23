f599364fb44d0fc6d4c9e005ba7b1b7d1bee7424
BHUMI1205 <158130392+BHUMI1205@users.noreply.github.com>
2024-09-23 10:08:44 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGO_URI); 
+await mongoose.connect(process.env.MONGO_URI); 
 // await mongoose.connect('mongodb://127.0.0.1/Blog');
 
 const db = mongoose.connection; 
